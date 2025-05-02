f537861998dc794aa5f19c8c8c18525775b8595c
Peepers22 <lorenzabad214@gmail.com>
2025-05-02 19:06:03 +0800

    commit

--- necromancer.java
+++ necromancer.java
@@ -1,7 +1,20 @@
 public class necromancer {
+    private String playername;
+
+    //constructor playername
+    public necromancer(String name){
+        this.playername = name;
+    
+    }
     
     //when the player pick the necromancer
-    public pick(){
+    public void introduction() throws InterruptedException{
+        Thread.sleep(2000);
+        System.out.println("So you have been practicing the language of the dead master, the destroyer of life!!");
+        Thread.sleep(1000);
+        System.out.println("I am glad to see you and have joined the dark side!!, master " + playername);
+    }
+    public void stats(){
     
     }
 }
--- start.java
+++ start.java
@@ -1,23 +1,46 @@
 
 import java.util.Scanner;
 public class start {
-//necromancer necro = new necromancer();
 Scanner sc = new Scanner(System.in);
 String playerclass;
-String playername;
+private String playername;
+private necromancer necro;
 
 
+
+//Choosing class and name
 public void chooseyourclass() throws InterruptedException {
-StringBuilder name = new StringBuilder("Ohh... welcome back master _____!");
+//intro name
+StringBuilder name = new StringBuilder("Ohh... welcome back Master errm..?");
 System.out.println("Hello Welcome to lostworld!");
 Thread.sleep(3000);
 System.out.println(name);
 Thread.sleep(1000);
+System.out.println("-----------------------");
 System.out.print("Enter your Name: ");
 playername = sc.nextLine();
-name.replace(27, 32, playername);
-System.out.println(name);
+name.replace(27, 34, playername);
+Thread.sleep(500);
+System.out.println("...");
+Thread.sleep(500);
+System.out.println("...");
+Thread.sleep(500);
+System.out.println("...");
+Thread.sleep(500);
+System.out.println("...");
+Thread.sleep(500);
+System.out.println(name +"!");
 
+necro = new necromancer(playername);
+necro.introduction();
+
+
+
+}
+
+//playername getter
+public String getPlayerName(){
+    return playername;
 }
 
 
